2398e6290d1c225c0d9d26da8bfde2c9f495ac5b
BenjaminPLee <yardspoon@gmail.com>
2016-07-28 11:43:32 -0500

    v1 - Setup additional presets (BL/EH)

--- app/src/main/java/com/yardspoon/sendaway/MainActivity.java
+++ app/src/main/java/com/yardspoon/sendaway/MainActivity.java
@@ -26,24 +26,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }.execute();
 
-        findViewById(R.id.go_to_bismark).setOnClickListener(new View.OnClickListener() {
+        setupClickListener(R.id.go_to_louisville, 38.199135, -85.5736522);
+        setupClickListener(R.id.go_to_stl, 38.633387, -90.2470689);
+        setupClickListener(R.id.go_to_peterborough, 46.819931, 100.7985263);
+        setupClickListener(R.id.go_to_salluit, 62.2047572, -75.6512441);
+        setupClickListener(R.id.go_to_honolulu, 21.3543173, -157.9438843);
+        setupClickListener(R.id.go_to_bismark, 46.819931, 100.7985263);
+        setupClickListener(R.id.go_to_sandiego, 32.841244, 117.281568);
+        setupClickListener(R.id.go_to_dayton, 39.740528, 84.1818745);
+    }
+
+    private void setupClickListener(int id, final double aLat, final double aLong) {
+        findViewById(id).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View view) {
-                changeLocation(46.819931, -100.7985263);
-            }
-        });
-
-
-        findViewById(R.id.go_to_sandiego).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                changeLocation(32.841244, -117.281568);
-            }
-        });
-
-
-        findViewById(R.id.go_to_dayton).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View view) {
-                changeLocation(39.740528, -84.1818745);
-            }
+                changeLocation(aLat, -aLong); }
         });
     }
 
