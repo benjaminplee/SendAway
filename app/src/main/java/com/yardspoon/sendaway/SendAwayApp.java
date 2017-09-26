ba95496c76be66d5b62efaf5a18fe3604bc9d512
Benjamin P Lee <benjaminplee@gmail.com>
2017-09-25 21:50:49 -0500

    setup additional build and analytic tracking

--- app/src/main/java/com/yardspoon/sendaway/SendAwayApp.java
+++ app/src/main/java/com/yardspoon/sendaway/SendAwayApp.java
@@ -3,7 +3,10 @@ package com.yardspoon.sendaway;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.answers.CustomEvent;
 
+import hu.supercluster.paperwork.Paperwork;
 import io.fabric.sdk.android.Fabric;
 
 
@@ -11,6 +14,13 @@ public class SendAwayApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Paperwork paperwork = new Paperwork(this);
+
         Fabric.with(this, new Crashlytics());
+        Crashlytics.setString("gitSHA", paperwork.get("gitSHA"));
+        Crashlytics.setString("buildTime", paperwork.get("buildTime"));
+
+        Answers.getInstance().logCustom(new CustomEvent("Application Create").putCustomAttribute("gitSHA", paperwork.get("gitSHA")));
     }
 }
